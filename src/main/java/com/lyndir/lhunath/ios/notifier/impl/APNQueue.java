cf052f1758fa322a01d1542f1ba7c5fa5aa3e041
Maarten Billemont <lhunath@gmail.com>
2011-04-18 15:19:35 +0200

    Clean up logging, use executors and make queue more robust.
    [IMPROVED]  Clean up debug-level logging.
    [IMPROVED]  Schedule off-thread code via an executor service.
    [IMPROVED]  Make the APN queue more robust by catching unexpected
                throwables that would otherwise kill the thread.

--- src/main/java/com/lyndir/lhunath/ios/notifier/impl/APNQueue.java
+++ src/main/java/com/lyndir/lhunath/ios/notifier/impl/APNQueue.java
@@ -26,15 +26,15 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * <h2>{@link APNQueue}<br> <sub>A queue which manages dispatching of notifications to the APNs.</sub></h2>
- *
+ * <p/>
  * <p> This queue collects notifications and dispatches them to the Apple Push Notification server using an {@link APNClient}. It is
  * designed to gather multiple notifications and dispatch them on the same connection to the APNs. </p>
- *
+ * <p/>
  * <p> As soon as a notification arrives, a connection to the APNs is established by the {@link APNClient}. The {@link APNQueue} then waits
  * for more notifications to arrive and dispatches each over this existing connection. After a configurable timeout (which defaults to
  * {@value #DEFAULT_TIMEOUT} milliseconds) of not receiving any additional notifications, the {@link APNQueue} shuts down {@link APNClient}
  * 's connection to the APNs and waits for more notifications in silence. When more arrive, this cycle begins anew. </p>
- *
+ * <p/>
  * <p> <i>Jun 30, 2009</i> </p>
  *
  * @author lhunath
@@ -45,13 +45,13 @@ public class APNQueue extends LinkedBlockingQueue<ByteBuffer> implements Runnabl
 
     private static final Logger logger = Logger.get( APNQueue.class );
 
-    protected static final long DEFAULT_TIMEOUT = 10 * 1000 /* By default, wait 10s before closing the APNs link. */;
+    protected static final long DEFAULT_TIMEOUT = 10 * 60 * 1000 /* By default, wait 10 min before closing the APNs link. */;
 
     private final APNClient apnClient;
 
     private long timeout = DEFAULT_TIMEOUT;
     private boolean running;
-    private Thread apnQueueThread;
+    private Thread  apnQueueThread;
 
     /**
      * Create a new {@link APNQueue} instance.
@@ -121,12 +121,16 @@ public class APNQueue extends LinkedBlockingQueue<ByteBuffer> implements Runnabl
             catch (NoSuchAlgorithmException e) {
                 logger.bug( e, "Could not initialize transport security: keys algorithms unsupported?" );
             }
+            catch (Throwable t) {
+                logger.err( t, "Caught unexpected throwable to save the APN queue thread." );
+            }
 
         logger.inf( "APNQueue has been shut down." );
     }
 
     /**
-     * Start the Apple Push Notification Queue so that it will start processing queued buffers and dispatching them to the {@link APNClient}
+     * Start the Apple Push Notification Queue so that it will start processing queued buffers and dispatching them to the {@link
+     * APNClient}
      * for submission.
      */
     public synchronized void start() {
